5bb297403960f59ee0c5f505dcb1263278f434e1
Anton <gumb1t97@gmail.com>
2022-06-21 23:58:52 +0300

    Added Client application

--- GrpcServer/Services/GreeterService.cs
+++ GrpcServer/Services/GreeterService.cs
@@ -6,6 +6,7 @@ namespace GrpcServer.Services;
 public class GreeterService : Greeter.GreeterBase
 {
     private readonly ILogger<GreeterService> _logger;
+
     public GreeterService(ILogger<GreeterService> logger)
     {
         _logger = logger;
@@ -15,7 +16,7 @@ public class GreeterService : Greeter.GreeterBase
     {
         return Task.FromResult(new HelloReply
         {
-            Message = "Hello " + request.Name
+            Message = "Hello " + request.Test
         });
     }
 }
